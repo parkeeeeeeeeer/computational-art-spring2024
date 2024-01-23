dafe7b57714c79b8cd2cfcb901a6f0a8a8bc6bc9
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-01-23 11:52:58 -0500

    Created the repository for comp-art

---
+++ assignments/01_portrait/sketch.js
@@ -0,0 +1,16 @@
+let width = 20;
+let length = 20;
+let height =20;
+
+position = createVector((width)/2,(height)/2);
+position = createVector((width)/2,(height)/2);
+
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(0,0,100);
+  
+}
