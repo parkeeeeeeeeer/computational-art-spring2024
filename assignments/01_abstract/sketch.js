53ed22f32553a925e339cd7f9cb491d2869cd095
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-01-27 16:02:52 -0500

    Current version of abstraction

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,32 +1,45 @@
-let portrait;
-let newImage = new Image();
-newImage.src = 'C:/Users/parke/.vscode/computational-art-spring2024/assignments/01_portrait/self.jpg';
+let things = [];
+let numThings = 20;
 
-let x = 20;
-let y =20;
+let x=0;
+let y=0;
 
+
+// p5 calls this function right away when the webpage is loaded
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(windowWidth, windowHeight);
 
-  colorMode(HSB);
-  portrait = new Portrait(x,y);
+  for(let i =0; i< numThings;i++){
+    things[i]= new Line(x,y,i);
+  }
+
 }
 
+// The draw function is called over and over again really fast by p5
 function draw() {
-  background(0,0,100);
+  background(255);
+  noFill();
   
-  portrait.update();
+  for(let i =0; i< things.length;i++){
+    things[i].update();
+  }
+//   x+=1;
+//   y+=1;
+//   sinX=sin(x);
+//   sinY=sin(y);
+//   rect(sinX+200,sinY+200,50,50);
 }
 
-class Portrait{
-  constructor(x,y){
-    this.hue=0;
-    this.position=createVector(x,y);
-    this.velocity= createVector(random(-5,5),random(-5,5));
+class Line{
+  constructor(x,y,count){
+    this.count=count;
+    this.position = createVector(x,y);
+    this.velocity = createVector(random(-5,5),random(-5,5));
   }
 
   update(){
     this.position.add(this.velocity);
+
     if (this.position.x > width || this.position.x < 0) {
       this.velocity.x *= -1;
     }
@@ -34,11 +47,30 @@ class Portrait{
     if (this.position.y > height || this.position.y < 0) {
       this.velocity.y *= -1;
     }
-  
-    this.hue += 5; // hue = hue + 1;
-    let saturation = mouseX / width * 100;
-  
-    fill(this.hue % 360, saturation, 100);
-    newImage(this.position.x, this.position.y, 100);
+
+    rotate(sin(frameCount+this.count)*100)
+    rect(0,0,600-this.position.x*3,600-this.count*3,200-this.count);
+    stroke(random(255));
   }
 }
+
+
+/**
+ * function draw() {
+  background(255);
+  noFill();
+  // translate(width/2,height/2);
+  for(let i =0; i< numSquares;i++){
+    
+    rotate(sin(frameCount+i)*100)
+    rect(0,0,600-i*3,600-i*3,200-i);
+    stroke(random(255));
+    
+  }
+//   x+=1;
+//   y+=1;
+//   sinX=sin(x);
+//   sinY=sin(y);
+//   rect(sinX+200,sinY+200,50,50);
+}
+ */
