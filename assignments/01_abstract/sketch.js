e8d10c2a7ce974d7ae689d1ffe3cdf4862d034fd
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-01-29 12:52:21 -0500

    Added final comments

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,3 +1,4 @@
+// Global Variables 
 let things = [];
 let numThings = 5000;
 
@@ -8,7 +9,8 @@ let y=0;
 // p5 calls this function right away when the webpage is loaded
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  // make numThings amounts of lines by constructing a new object and setting it
+ // to i element in the array of things
   for(let i =0; i< numThings;i++){
     things[i]= new Line(x,y,i);
   }
@@ -23,13 +25,8 @@ function draw() {
   for(let i =0; i< things.length;i+=1){
     things[i].update();
   }
-//   x+=1;
-//   y+=1;
-//   sinX=sin(x);
-//   sinY=sin(y);
-//   rect(sinX+200,sinY+200,50,50);
 }
-
+// This class uses the ellipse shape with only the outline. 
 class Line{
   constructor(x,y,count){
     this.count=count;
@@ -39,7 +36,7 @@ class Line{
 
   update(){
     this.position.add(this.velocity);
-
+    // This code is from 1/22/2024 class.
     if (this.position.x > width || this.position.x < 0) {
       this.velocity.x *= -1;
     }
@@ -47,26 +44,14 @@ class Line{
     if (this.position.y > height || this.position.y < 0) {
       this.velocity.y *= -1;
     }
-
+    // rotating the ellipse by i.
     rotate(sin(frameCount+this.count))
+    // Using the position of x and y to vary the shape and place of the ellipse.
     ellipse(windowWidth,windowHeight,600-this.position.x*3,600-this.position.y*3,200-this.count);
+    // This makes the range of color mostly blue and black
     stroke(random(50),0,random(122,255));
   }
 }
-
-
-/**
- * function draw() {
-  background(255);
-  noFill();
-  // translate(width/2,height/2);
-  for(let i =0; i< numSquares;i++){
-    
-    rotate(sin(frameCount+i)*100)
-    rect(0,0,600-i*3,600-i*3,200-i);
-    stroke(random(255));
-    
-  }
 //   x+=1;
 //   y+=1;
 //   sinX=sin(x);
