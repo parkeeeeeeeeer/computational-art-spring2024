e141deec18ec0d2556f895668911a4cb473bd0cd
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-02-08 12:54:15 -0500

    cleanup

--- assignments/02_noise/sketch.js
+++ assignments/02_noise/sketch.js
@@ -2,38 +2,42 @@
 let slider;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  // Created slider
   slider = createSlider(500,10000);
   slider.position(width/2, 10);
-  slider.size(80);
+  slider.size(200);
 }
 
 function draw() {
     background(255);
     translate(width/2,height/2);
-  
-    // background(g);
 
     let startSize = 200;
     let t=0;
 
-    r = random(255); // r is a random number between 0 - 255
-    g = random(100,200); // g is a random number betwen 100 - 200
-    b = random(100); // b is a random number between 0 - 100
-    a = random(200,255); // a is a random number between 200 - 255
-
     for(let i =0;i<360;i+=1){
       let n = noise(t);
       let color = map(n,0,1,0,255);
+      // making it just the lines
       noFill();
+      // mapping noise to the stroke color
       stroke(31, 40, color);
-      // triangle(0,0,0,startSize*n,startSize*n, startSize*n);
+      // Using noice and 4 to make a new triangle
       triangle(0,0,0,startSize*n,startSize*n, startSize*t);
-      // rotate(frameCount/random(200,5000)); 
-
+      
+      
+      // speed based on slider val
       rotate(frameCount/slider.value()); 
-      t+=.005
+      t+=.005;
     }
 
 }
+
+
+// Extra
+// rotate(frameCount/random(200,5000)); 
+// triangle(0,0,0,startSize*n,startSize*n, startSize*n);
+
 
 
