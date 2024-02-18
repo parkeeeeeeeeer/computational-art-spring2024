5aa0bd0365377ff0d5b67e42d08441eab526bad5
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-02-18 07:23:52 -0500

    current

--- assignments/03_physics/sketch.js
+++ assignments/03_physics/sketch.js
@@ -1,67 +1,91 @@
 
 let slider;
+
+let pens=[];
+let numPen =5;
+let stringLen=200;
+let size =50;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for(let i=0;i<numPen;i++){
+    pens[i] = new Pen(i*size,0,stringLen,size);
+  }
   
-  // Created slider
-  slider = createSlider(0,100);
-  slider.position(width/2, 10);
-  slider.size(200);
-  angleMode(DEGREES);
 }
 
 function draw() {
     background(255);
-    translate(width/2,height/2);
-    
-    let size = random(500);
-
-    noFill()
-  
-  
-    for (var i = 0; i < 50; i++) {
-  
-      rotate(frameCount / slider.value());
-  
-      beginShape()
-      for (var j = 0; j < 360; j += size) {
-        var rad = i * 3;
-        var x = rad * cos(j);
-        var y = rad * sin(j);
-        var z = sin(frameCount * 2 + i * 5) * 50;
-  
-        vertex(x, y);
-      }
-      endShape(CLOSE)
+    translate(width/2.5,height/4);
+    line(0,0,(size*(numPen-1)),0)
+    for(let pen of pens){
+      pen.update();
+      pen.make();
     }
-
-
+    
+    
 }
 
-function shape(length,num){
-  noFill();
-  beginShape();
-  for(let i =0;i<359;i+=360/num){
-   var x = length* cos(i);
-   var y = length* sin(i);
-   vertex(x,y); 
+function mousePressed(){
+  for(let pen of pens){
+    pen.clicked(mouseX,mouseY);
+    
   }
-  endShape(CLOSE);
+
 }
-function curvedShape(length,num){
-  noFill();
-  beginShape();
-  for(let i =0;i<359;i+=360/num){
-   var x = length* cos(i);
-   var y = length* sin(i);
-   curveVertex(x,y); 
+// Code used from the Nature of Code https://editor.p5js.org/natureofcode/sketches/MQZWruTlD
+class Pen{
+
+  constructor(x,y,r,size){
+    this.origin = createVector(x,y);
+    this.pos = createVector();
+    this.r = r;
+    this.angle=PI/4;
+    this.angleA=0.0;
+    this.angleV=0.0;
+    this.dampen=0.995;
+    this.ballSize=size;
+    this.dragging =false;
   }
-  endShape(CLOSE);
+
+  make(){
+    this.pos.set(this.r * sin(this.angle), this.r * cos(this.angle), 0); 
+    this.pos.add(this.origin);
+    
+    line(this.origin.x,this.origin.y,this.pos.x,this.pos.y);
+    if (this.dragging) fill(200);
+    circle(this.pos.x,this.pos.y,this.ballSize);
+  }
+  update(){
+    if(!this.dragging){
+      let gravity =0.4;
+      this.angleA=((-1*gravity)/this.r) *sin(this.angle);
+      this.angleV+= this.angleA;
+      this.angle+=this.angleV;
+      this.angleV*=this.dampen;
+    }
+  }
+
+  clicked(mx, my) {
+    let d = dist(mx, my, this.pos.x, this.pos.y);
+    if (d < this.ballSize) {
+      this.dragging = true;
+    }
+  }
+
+  // This tells us we are not longer clicking on the ball
+  stopDragging() {
+    this.angleV = 0; // No velocity once you let go
+    this.dragging = false;
+  }
+
+  drag() {
+    // If we are draging the ball, we calculate the angle between the
+    // pendulum origin and mouse position
+    // we assign that angle to the pendulum
+    if (this.dragging) {
+      let diff = p5.Vector.sub(this.pivot, createVector(mouseX, mouseY)); // Difference between 2 points
+      this.angle = atan2(-1 * diff.y, diff.x) - radians(90); // Angle relative to vertical axis
+    }
+  }
 }
-
-// Extra
-// rotate(frameCount/random(200,5000)); 
-// triangle(0,0,0,startSize*n,startSize*n, startSize*n);
-
-
-
