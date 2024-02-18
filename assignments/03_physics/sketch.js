3cc906e9f9b5fc2af7a3e1e3bfdb3629ea24c821
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-02-18 08:48:58 -0500

    more motion

--- assignments/03_physics/sketch.js
+++ assignments/03_physics/sketch.js
@@ -2,22 +2,23 @@
 let slider;
 
 let pens=[];
-let numPen =5;
-let stringLen=200;
-let size =50;
+let numPen =50;
+let stringLen=25;
+let size=25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
   for(let i=0;i<numPen;i++){
-    pens[i] = new Pen(i*size,0,stringLen,size);
+    pens[i] = new Pen(0,0,stringLen*i,size);
   }
   
 }
 
 function draw() {
     background(255);
-    translate(width/2.5,height/4);
-    line(0,0,(size*(numPen-1)),0)
+    translate(width/2,0);
+
     for(let pen of pens){
       pen.update();
       pen.make();
@@ -57,35 +58,11 @@ class Pen{
     circle(this.pos.x,this.pos.y,this.ballSize);
   }
   update(){
-    if(!this.dragging){
-      let gravity =0.4;
-      this.angleA=((-1*gravity)/this.r) *sin(this.angle);
-      this.angleV+= this.angleA;
-      this.angle+=this.angleV;
-      this.angleV*=this.dampen;
-    }
-  }
-
-  clicked(mx, my) {
-    let d = dist(mx, my, this.pos.x, this.pos.y);
-    if (d < this.ballSize) {
-      this.dragging = true;
-    }
-  }
-
-  // This tells us we are not longer clicking on the ball
-  stopDragging() {
-    this.angleV = 0; // No velocity once you let go
-    this.dragging = false;
-  }
-
-  drag() {
-    // If we are draging the ball, we calculate the angle between the
-    // pendulum origin and mouse position
-    // we assign that angle to the pendulum
-    if (this.dragging) {
-      let diff = p5.Vector.sub(this.pivot, createVector(mouseX, mouseY)); // Difference between 2 points
-      this.angle = atan2(-1 * diff.y, diff.x) - radians(90); // Angle relative to vertical axis
-    }
+    let gravity =0.4;
+    this.angleA=((-1*gravity)/this.r) *sin(this.angle);
+    this.angleV+= this.angleA;
+    this.angle+=this.angleV;
+    this.angleV*=this.dampen;
+  
   }
 }
