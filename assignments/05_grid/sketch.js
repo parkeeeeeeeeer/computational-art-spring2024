cd9469812350b776f684bebedad24644cc593196
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-02-29 10:31:31 -0500

    finished 05 grid

--- assignments/05_grid/sketch.js
+++ assignments/05_grid/sketch.js
@@ -4,18 +4,19 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+    background(0,0,0);
     // Setting noise for the y
     let yt=0;
     // Length of each square
     let length = 50;;
     // Creating Grid
-
+    shape(1,1);
     for(let y=0;y<innerHeight;y+=length){
         // Setting noise for the x
         let xt=0;
         for(let x=0;x<width;x+=length){
             let r = noise(xt, yt) * 255;
+            stroke(255,255,225);
             rect(x,y,length,length);
             push();
             translate(x+length/2,y+length/2);
@@ -39,13 +40,16 @@ function shape(length,num){
     }
     endShape(CLOSE);
   }
+//   Making a shape that is with curves
   function curvedShape(length,num){
     noFill();
     beginShape();
     for(let i =0;i<360;i+=360/num){
      var x = length* cos(i);
+    //  Usage of Sin
      var y = length* sin(i);
      curveVertex(x,y); 
     }
     endShape(CLOSE);
   }
+  
