9cc5f2c327c62afbd191c183258ab6b433e9249e
parkeeeeeeeeer <46697381+parkeeeeeeeeer@users.noreply.github.com>
2024-05-01 18:56:38 -0400

    Current

--- assignments/final/README.md
+++ assignments/final/README.md
@@ -6,6 +6,16 @@ LINK: Your project should be accessible by going to the link referenced in the â
 
 FIVE INTERCONNECTED ASPECTS: You need to specifically list the five aspects of your system that you made connect to one another. Which one do you tihnk produced the most interesting effect?
 
+1. If mouse is pressed then sound loop will 
+2. If the mouse is pressed when sound loop  is going then it will stop playing the noise
+3. The amplitude is connected to the height of the boxes
 
 MULTIMEDIA: Specifically point out the three (or more) types of multimedia you used.
+
+1. 3D boxes are being useed
+2. Sound is being used 
+3. 
+
 ALLOWED TO PUBLICLY SHARE?: Let me know if you are willing to have me use your project in a public demonstration of student work in DMTI.
+
+I am alright with this being shared.
--- assignments/final/sketch.js
+++ assignments/final/sketch.js
@@ -1,81 +1,88 @@
 let terrain
+let img
+let backImg
+let imgWidth;
+let imgHeight
 
-
-let scl = 30
+let scl = 50
 
 let mic, fft
 let song
 
 function preload() {
   // Load your song here
-  song = loadSound('cord.wav')
+  song = loadSound('wave.wav')
+  img = loadImage('water.jpg')
+  // backImg = loadImage('dusk.png')
+
 }
 
 function setup() {
     createCanvas(400,400, WEBGL)
     angleMode(DEGREES)
-
-    noiseDetail(1)
-
-    fft = new p5.FFT();
-    fft.setInput(song);
+    noiseDetail(2)
+      
+    amp = new p5.Amplitude()
+    amp.setInput(song)
+  
+    fft = new p5.FFT()
+    fft.setInput(song)
   
     // Play the song
-    song.play();
+    // song.play();
 //     terrain = new Terrain(scl,0);
 }
 
 function draw() {
     background(255)
+    // image(backImg,-width/2,-height+170)
+    
 
-    let spectrum = fft.analyze()
+    // img.loadPixels()
 
-
-    // translate((-width/2)+scl/2,(-height/2)+scl/2,-400)
-
-    // translate(-width/2,-height/2,-400)
-
-    // for(let x=0;x<cols;x++){
-    //   for(let y=0;y<rows;y++){
-    //       push()
-    //       translate(x*scl,y*scl,0)
-    //       box(scl)
-    //       pop()
-      
-    //   }
-    // }
     let xOff=0
     let index = 0
-    let start = frameCount *0.01
-
+    let start = frameCount *.01
+    let level = amp.getLevel();
     console.log(start)
-
-
-    translate(0,0,-400)
-    rotateX(80)
+    noStroke()
+    fill(255)
+    // noFill()
+    translate(-100,120,-200)
+    rotateX(70)
+    rotateZ(90)
     
+   
+    
+
     for(let x=(-width/2);x<=width/2;x+=scl){
       let yOff=0
       for(let y=(-height);y<=height/2;y+=scl){
-        let h= map(spectrum[index], 0, 255, -scl * 2, 0)
-        // let h = map(noise(xOff+start,yOff+start),0,1,-scl*2,scl*2)
+        let h= map(noise(xOff+start,yOff+start),0,1,-scl*level*15,-scl)
+        // let h = map(sin(xOff + start) * cos(yOff + start), -1, 1, -scl * 5, scl) * level
+        
+        
+        // let r = map(x,-width/2,width/2,0,255)
+        // let g = map(y,-height/2,height/2,0,255)
+        // let b = map(h,-scl*2,0,0,255)
+
+
+        // fill(r, g, b, alpha);
+
         push()
+        // fill(r,g,b)
         translate(x,y,-h/2)
+        texture(img);
         box(scl,scl,h)
         pop()
 
         yOff+=.1
-        index += 1
         
       }
       xOff+=.1
       
      
     }
-
-    // terrain.display();
-
-    
 }
 
 function mousePressed(){
@@ -83,7 +90,21 @@ function mousePressed(){
   if (song.isPlaying()) {
     song.pause(); // Pause the song if it's playing
   } else {
-    song.play(); // Otherwise, play the song
+    song.loop(); // Otherwise, play the song
   }
 
 }
+
+
+
+        // // let index = (x + y * img.width) * 4;
+        // // let r = img.pixels[index];
+        // // let g = img.pixels[index+1];
+        // // let b = img.pixels[index+2];
+        // // let a = img.pixels[index+3];
+
+        // let avg = (r+g+b) / 3;
+        // let alpha = 255;
+        // // if (avg > 120) {
+        // //   alpha = 0;
+        // // }
